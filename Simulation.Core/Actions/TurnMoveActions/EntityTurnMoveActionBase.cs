3cb4c7348e196be999bac1204c4b186b3a07b24d
Bagaev Sergei <a1f4pvp@yandex.ru>
2024-09-06 18:02:52 +0500

    Project update.

---
+++ Simulation.Core/Actions/TurnMoveActions/EntityTurnMoveActionBase.cs
@@ -0,0 +1,9 @@
+﻿using Simulation.Core.Interfaces;
+using Simulation.Core.Settings;
+
+namespace Simulation.Core.Actions.TurnMoveActions;
+public abstract class TurnMoveCreatureActionBase : IAction
+{
+    public abstract void Perform(IMap map, SimulationSettings simulationSettings);
+    protected abstract void MakeTurnMove(IMap map, SimulationSettings simulationSettings);
+}

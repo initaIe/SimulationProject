40ec28ecc37c5f97b7cc894ed2dd7c39a013cb35
Bagaev Sergei <a1f4pvp@yandex.ru>
2024-09-06 19:10:17 +0500

    Create exemplars.

--- Simulation.ConsoleApp/Program.cs
+++ Simulation.ConsoleApp/Program.cs
@@ -1,4 +1,56 @@
-﻿using System.Threading.Channels;
+﻿using Simulation.Core.Entities;
+using Simulation.Core.Implementations;
+using Simulation.Core.Settings;
+using Simulation.Core.Settings.Entity.Attributes;
+using Simulation.Core.Settings.Entity.Implementations;
 
 Console.OutputEncoding = System.Text.Encoding.UTF8;
-Console.WriteLine("🐺");
+
+
+var map = new Map();
+
+var consoleFieldRender = new FieldConsoleRenderer();
+
+var turnTracker = new TurnTracker();
+
+HashSet<string> staticObjectSprites = ["⛰️", "🌳"];
+
+var staticObjectSettings = new StaticObjectSettings(
+    new LimitSettings(10, 20),
+    new DisplaySettings(staticObjectSprites));
+
+HashSet<string> foodSprites = ["🍖", "🍕"];
+
+var foodSettings = new FoodSettings(
+    new LimitSettings(5, 10),
+    new DisplaySettings(foodSprites),
+    new LimitSettings(2, 4));
+
+HashSet<string> herbivoreSprites = ["🐑", "🐖"];
+
+HashSet<Type> herbivorePreys = [typeof(Food)];
+
+var herbivoreSettings = new HerbivoreSettings(
+    new LimitSettings(5, 10),
+    new DisplaySettings(herbivoreSprites),
+    new LimitSettings(5, 10),
+    new LimitSettings(1, 2),
+    new PreySettings(herbivorePreys));
+
+HashSet<string> predatorSprites = ["🐺", "🦅"];
+
+HashSet<Type> predatorPreys = [typeof(Food), typeof(Herbivore)];
+
+var predatorSettings = new PredatorSettings(
+    new LimitSettings(4, 6),
+    new DisplaySettings(herbivoreSprites),
+    new LimitSettings(10, 15),
+    new LimitSettings(2, 3),
+    new PreySettings(predatorPreys),
+    new LimitSettings(4, 6));
+
+
+var entitiesSettings = new EntitiesSettings(staticObjectSettings, foodSettings, herbivoreSettings, predatorSettings);
+
+var simulationSettings = new SimulationSettings();
+var simulation = new Simulation.Core.Simulation();
--- Simulation.Core/Simulation.cs
+++ Simulation.Core/Simulation.cs
@@ -1,13 +1,40 @@
-﻿using Simulation.Core.Interfaces;
+﻿using Simulation.Core.Actions;
+using Simulation.Core.Actions.CreationActions;
+using Simulation.Core.Interfaces;
+using Simulation.Core.POCOs;
 using Simulation.Core.Settings;
+using Simulation.Core.Utilities;
 
 namespace Simulation.Core;
-public class Simulation(IMap map, IFieldRender fieldRender, ITurnTracker turnTracker, SimulationSettings settings)
+public class Simulation(
+    IMap map,
+    IFieldRender fieldRender,
+    ITurnTracker turnTracker,
+    SimulationSettings settings)
 {
     private readonly IMap _map = map;
     private readonly IFieldRender _fieldRender = fieldRender;
     private readonly ITurnTracker _turnTracker = turnTracker;
     private readonly SimulationSettings _settings = settings;
-    
-    
+    private readonly List<IAction> _actions = [];
+    public void Start()
+    {
+        CreateActions();
+        while (true)
+        {
+            _actions.ForEach(a => a.Perform(_map, _settings));
+            _fieldRender.Render(_settings.Field, ConsoleRenderUtils.GetRenderEntityData(_map));
+            Thread.Sleep(500);
+        }
+    }
+    private void CreateActions()
+    {
+        _actions.Add(new StaticObjectCreationAction());
+        _actions.Add(new FoodCreationAction());
+        _actions.Add(new HerbivoreCreationAction());
+        _actions.Add(new PredatorCreationAction());
+        
+        _actions.Add(new HerbivoreCreationAction());
+        _actions.Add(new PredatorCreationAction());
+    }
 }

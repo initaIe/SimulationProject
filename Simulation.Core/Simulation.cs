1cafcc2271774d75de4fa0de2fe224e2006be428
Bagaev Sergei <a1f4pvp@yandex.ru>
2024-09-06 18:10:55 +0500

    no message

--- Simulation.Core/Simulation.cs
+++ Simulation.Core/Simulation.cs
@@ -2,14 +2,12 @@
 using Simulation.Core.Settings;
 
 namespace Simulation.Core;
-public class Simulation(
-    IFieldRender fieldRender,
-    ITurnTracker turnTracker,
-    SimulationSettings simulationSettings,
-    IMap map)
+public class Simulation(IMap map, IFieldRender fieldRender, ITurnTracker turnTracker, SimulationSettings settings)
 {
     private readonly IMap _map = map;
-    private readonly IFieldRender _mapRender = fieldRender;
+    private readonly IFieldRender _fieldRender = fieldRender;
     private readonly ITurnTracker _turnTracker = turnTracker;
-    private readonly SimulationSettings _simulationSettings = simulationSettings;
+    private readonly SimulationSettings _settings = settings;
+    
+    
 }
